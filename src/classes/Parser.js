3d4b9b4e0a79ca10ac5599b14791d95702310088
Jakub Bystroński <jbstr85@gmail.com>
2022-05-05 14:22:34 +0200

    Refactored Parser.js

--- src/classes/Parser.js
+++ src/classes/Parser.js
@@ -1,19 +1,20 @@
-const ParserError = require("./../errors/ParserError");
+const ErrorHandler = require("../errors/ErrorHandler");
 
 module.exports = class Parser {
   constructor(params = null, bodyObject = null) {
     this.parserQueue = [];
+    this.precedenceParsersQueue = [];
+    this.afterParsersQueue = [];
     this.filtersObject = {};
     this.queryObject = {};
     this.bodyObject = bodyObject;
     this.params = params;
 
-    this.parserKeywords = {
-      ASC: 1,
-      DESC: -1
-    };
-
     this.parseFunctions = [
+      "_date",
+      "_nor",
+      "_and",
+      "_or",
       "_sort",
       "_limit",
       "_skip",
@@ -21,6 +22,23 @@ module.exports = class Parser {
       "_except",
       "_set"
     ];
+
+    this.precedenceParsers = ["_or", "_nor", "_and"];
+    this.afterParsers = [
+      "_date",
+      "_sort",
+      "_skip",
+      "_limit",
+      "_only",
+      "_except",
+      "_set"
+    ];
+
+    this.arrayKeywords = ["_in", "_not_in", "_only", "_except", "_array_all"];
+  }
+
+  getError(e) {
+    return new ErrorHandler(e);
   }
 
   isOperator(k) {
@@ -30,37 +48,28 @@ module.exports = class Parser {
   isRegularObject(v) {
     try {
       return v !== null && v.constructor.name === "Object";
-    } catch (error) {
-      throw new Error(error);
+    } catch (e) {
+      return this.getError(e);
     }
   }
 
   isType(v, type) {
-    return (
-      Object.prototype.toString.call(v).slice(8, -1).toLowerCase() === type
-    );
-  }
+    try {
+      const checkType = (v, t) =>
+        Object.prototype.toString.call(v).slice(8, -1).toLowerCase() === t;
 
-  isNestedProperty(key) {
-    return key.split(".").length > 1;
-  }
+      if (Array.isArray(type)) {
+        let matches = [];
 
-  parseNestedObject(key, value) {
-    const objectKeys = key.split(".");
+        matches = type.map((t) => checkType(v, t));
 
-    function nestObjects(container, props, value, counter = 0) {
-      if (props[counter + 1] === undefined) {
-        container[props[counter]] = value;
-        return container;
-      }
-      if (!container.hasOwnProperty(props[counter])) {
-        container[props[counter]] = {};
+        return matches.includes(true) ? true : false;
       }
 
-      nestObjects(container[props[counter]], props, value, ++counter);
+      return checkType(v, type);
+    } catch (e) {
+      return this.getError(e);
     }
-
-    nestObjects(this.queryObject, objectKeys, value);
   }
 
   isValueNumeric(str) {
@@ -94,54 +103,81 @@ module.exports = class Parser {
       }
 
       const forceToArray =
-        k &&
-        ["_in", "_not_in", "_only", "_except"].includes(
-          k.split(".").reverse()[0]
-        );
+        k && this.arrayKeywords.includes(k.split(".").reverse()[0]);
 
       return forceToArray ? values : values[0];
     } catch (e) {
-      console.error(e);
+      return this.getError(e);
     }
   }
 
   _refitKeys = (object) => {
-    return Array.isArray(object)
-      ? object.map(this._refitKeys)
-      : object && this.isRegularObject(object)
-      ? Object.fromEntries(
-          Object.entries(object).map(([k, v]) => [
-            this.keywords.hasOwnProperty(k) ? this.keywords[k] : k,
-            this._refitKeys(v)
-          ])
-        )
-      : object;
+    try {
+      return Array.isArray(object)
+        ? object.map(this._refitKeys)
+        : object && this.isRegularObject(object)
+        ? Object.fromEntries(
+            Object.entries(object).map(([k, v]) => [
+              this.keywords.hasOwnProperty(k) ? this.keywords[k] : k,
+              this._refitKeys(v)
+            ])
+          )
+        : object;
+    } catch (e) {
+      return this.getError(e);
+    }
   };
 
-  parseParams(params) {
+  objectFromString(keyparts, value = {}) {
+    return keyparts.reduceRight((acc, currentValue) => {
+      return { [currentValue]: acc };
+    }, value);
+  }
+
+  needsArray(k) {
+    return this.arrayKeywords.includes(k);
+  }
+
+  paramParser(params, container = {}) {
     try {
       for (const [index, param] of params.entries()) {
-        const [key, val] = param.split("=");
-        const valueFromString = this.unstringify(val, key);
+        let [k, v] = param.split("=");
+        const valueFromString = this.unstringify(v, k);
+        let kparts = k.split(".");
+        // console.log(kparts.reverse()[0]);
+        // v = this.needsArray(kparts.reverse()[0]) ? v.split(",") : v;
+        if (kparts.length === 1) {
+          container[kparts[0]] = valueFromString;
+        } else {
+          if (["_nor", "_or", "_and"].includes(kparts[0])) {
+            if (!container.hasOwnProperty(kparts[0])) {
+              container[kparts[0]] = [];
+            }
 
-        switch (true) {
-          case this.isNestedProperty(key):
-            this.parseNestedObject(key, valueFromString);
-            break;
-          default:
-            this.queryObject[key] = valueFromString;
+            container[kparts[0]] = [
+              ...container[kparts[0]],
+              this.objectFromString(kparts.slice(1), valueFromString)
+            ];
+          } else {
+            container[kparts[0]] = this.mergeObjects([
+              container[kparts[0]],
+              this.objectFromString(kparts.slice(1), valueFromString)
+            ]);
+          }
         }
       }
+      return container;
     } catch (e) {
-      throw new Error(e);
+      return this.getError(e);
     }
   }
 
   run() {
     try {
-      this.params && this.parseParams(this.params);
-
-      console.log("BODY OBJECT", this.bodyObject);
+      if (this.params) {
+        this.queryObject = this.paramParser(this.params);
+        console.log("parsed", this.queryObject);
+      }
 
       this.queryObject = this.bodyObject
         ? { ...this.queryObject, ...this.bodyObject }
@@ -149,11 +185,16 @@ module.exports = class Parser {
 
       delete this.bodyObject;
 
-      console.log("QUERY OBJECT", this.queryObject);
-
       Object.keys(this.queryObject).forEach((k) => {
-        this.parseFunctions.includes(k) && this.parserQueue.push(k);
+        this.precedenceParsers.includes(k) &&
+          this.precedenceParsersQueue.push(k);
+
+        this.afterParsers.includes(k) && this.afterParsersQueue.push(k);
       });
+
+      // Object.keys(this.queryObject).forEach((k) => {
+      //   this.afterParsers.includes(k) && this.afterParsersQueue.push(k);
+      // });
 
       // shift some functions from queries object to filters object
       for (const k of Object.keys(this.queryObject)) {
@@ -170,49 +211,56 @@ module.exports = class Parser {
         }
       }
     } catch (e) {
-      throw new ParserError(e);
+      return this.getError(e);
     }
   }
 
   mergeObjects(objectsArr) {
-    // Variables
-    let target = {};
+    try {
+      // Variables
+      let target = {};
 
-    let merger = (obj) => {
-      for (let prop in obj) {
-        if (obj.hasOwnProperty(prop)) {
-          if (Object.prototype.toString.call(obj[prop]) === "[object Object]") {
-            // If we're doing a deep merge and the property is an object
-            target[prop] = this.mergeObjects([target[prop], obj[prop]]);
-          } else {
-            // Otherwise, do a regular merge
-            target[prop] = obj[prop];
+      let merger = (obj) => {
+        for (let prop in obj) {
+          if (obj.hasOwnProperty(prop)) {
+            if (
+              Object.prototype.toString.call(obj[prop]) === "[object Object]"
+            ) {
+              // If we're doing a deep merge and the property is an object
+              target[prop] = this.mergeObjects([target[prop], obj[prop]]);
+            } else {
+              // Otherwise, do a regular merge
+              target[prop] = obj[prop];
+            }
           }
         }
+      };
+
+      //Loop through each object and conduct a merge
+      for (let i = 0; i < objectsArr.length; i++) {
+        merger(objectsArr[i]);
       }
-    };
 
-    //Loop through each object and conduct a merge
-    for (let i = 0; i < objectsArr.length; i++) {
-      merger(objectsArr[i]);
+      return target;
+    } catch (e) {
+      return this.getError(e);
     }
-
-    return target;
   }
 
   parametrizeBody(ob, q = "", queryParts = []) {
-    console.log("object before parametrization", ob);
-
-    for (const [k, v] of Object.entries(ob)) {
-      console.log("parametrize body", k);
-      if (v !== null && v.constructor.name === "Object") {
-        this.parametrizeBody(ob[k], q + k + ".", queryParts);
-      } else {
-        queryParts.push(`${q}${k}=${v}`);
+    try {
+      for (const [k, v] of Object.entries(ob)) {
+        if (v !== null && v.constructor.name === "Object") {
+          this.parametrizeBody(ob[k], q + k + ".", queryParts);
+        } else {
+          queryParts.push(`${q}${k}=${v}`);
+        }
       }
+
+      return queryParts;
+    } catch (e) {
+      return this.getError(e);
     }
-    console.log("queryParts", queryParts);
-    return queryParts;
   }
 
   getQueue() {
