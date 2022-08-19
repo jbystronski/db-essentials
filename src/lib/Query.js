658c3da40f29dd6c040e8c3e943e467e73ef4487
Jakub Bystroński <jbstr85@gmail.com>
2022-08-19 23:41:19 +0200

    Trying to move as much responsibilities away and make it control just the data flow

--- src/lib/Query.js
+++ src/lib/Query.js
@@ -4,34 +4,6 @@ const Filesystem = require("./Filesystem");
 
 const COLLECTION_INIT_ACTIONS = ["save_one", "save_many"];
 
-const CRUD_ACTIONS = {
-  save_one: "insert",
-  save_many: "insert",
-  find: "find",
-  find_one: "findOne",
-  update_one: "updateOne",
-  update_many: "updateMany",
-  delete_one: "deleteOne",
-  delete_many: "deleteMany",
-  count: "count",
-};
-
-async function dumpDocumentIds(collectionNames) {
-  try {
-    const container = [];
-
-    for (const name in collectionNames) {
-      const res = await this.run(`find/${modelNamesArray[name]}?_only=_id`);
-
-      container.push(res.map((el) => el._id));
-    }
-
-    return container;
-  } catch (e) {
-    console.error(e);
-  }
-}
-
 getEndpoint = (url) => getUrlSegments(url).slice(2).reverse().join("/");
 
 getUrlSegments = (url) => url.split("?")[0].split("/").reverse();
@@ -69,14 +41,12 @@ create = ({ connection, url, body = null }) => {
 
         const parser = Parser.create(collection, params, body);
 
-        await parser.run();
-
         try {
-          const { data, payload, error } = await parser[CRUD_ACTIONS[action]]();
+          const { data, payload, error } = await parser.run(action);
 
           data &&
             connection.mode === "persist" &&
-            Filesystem.persist(data, `${connection.db}/${table}.json`);
+            Filesystem.persist(data, `${connection.database}/${table}.json`);
 
           return payload;
         } catch (e) {
