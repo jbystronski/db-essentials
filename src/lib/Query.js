355e7fb44549c47ddbecb878cfe4b5ebd52a4a0e
Jakub Bystroński <jbstr85@gmail.com>
2022-08-20 02:37:55 +0200

    Decoupling, should be just a data flow controller

--- src/lib/Query.js
+++ src/lib/Query.js
@@ -2,15 +2,7 @@ const parseEncodedUri = require("../utils/parseEncodedUri");
 const Parser = require("./Parser");
 const Filesystem = require("./Filesystem");
 
-const COLLECTION_INIT_ACTIONS = ["save_one", "save_many"];
-
-getEndpoint = (url) => getUrlSegments(url).slice(2).reverse().join("/");
-
 getUrlSegments = (url) => url.split("?")[0].split("/").reverse();
-
-getAction = (url) => getUrlSegments(url)[1];
-
-getTable = (url) => getUrlSegments(url)[0];
 
 getParams = (url) => {
   const parametersString = url.split("?")[1];
@@ -18,35 +10,31 @@ getParams = (url) => {
   return parametersString ? parametersString.split("&") : null;
 };
 
-create = ({ connection, url, body = null }) => {
+create = ({ connection, url, body }) => {
   return {
     async run() {
       try {
-        if (!url) throw Exception(`Missing url, aborting`);
-        body = body && typeof body === "string" ? JSON.parse(body) : body;
+        if (!url) throw Error(`Missing url, aborting`);
 
         url = parseEncodedUri(url);
 
-        const action = getAction(url);
-
-        const table = getTable(url);
-
-        const params = getParams(url);
-
-        const collection = (await connection.getCollection(table)) || [];
-
-        if (!COLLECTION_INIT_ACTIONS.includes(action) && !collection.length) {
-          return `Collection ${table} doesn't exist`;
-        }
-
-        const parser = Parser.create(collection, params, body);
+        const parser = Parser.create(
+          (await connection.getCollection(getUrlSegments(url)[0])) || [],
+          getParams(url),
+          (body = body && typeof body === "string" ? JSON.parse(body) : body) // parse body object if present
+        );
 
         try {
-          const { data, payload, error } = await parser.run(action);
+          const { data, payload, error } = await parser.run(
+            getUrlSegments(url)[1] // url action
+          );
 
           data &&
             connection.mode === "persist" &&
-            Filesystem.persist(data, `${connection.database}/${table}.json`);
+            Filesystem.persist(
+              data,
+              `${connection.database}/${getUrlSegments(url)[0]}.json`
+            );
 
           return payload;
         } catch (e) {
