1ca2d8433a1d27dcd76c9660b1451508eaa6fcde
Jakub Bystroński <jbstr85@gmail.com>
2022-08-25 08:47:19 +0200

    Removed pointless array check on the query, which unneccesarily bloated the code

--- src/lib/InitialParsers.js
+++ src/lib/InitialParsers.js
@@ -1,47 +1,10 @@
 const { matchCondition } = require("../utils/filterData");
 
-exports._nor = ({ data, queries: { _nor } }) => {
-  Array.isArray(_nor)
-    ? _nor.forEach((condition) => {
-        data = data.filter(
-          (record) => !matchCondition(record, condition).includes(true)
-        );
-      })
-    : (data = data.filter(
-        (record) => !matchCondition(record, _nor).includes(true)
-      ));
+exports._nor = ({ data, queries: { _nor } }) =>
+  data.filter((record) => !matchCondition(record, _nor).includes(true));
 
-  return data;
-};
+exports._or = ({ data, queries: { _or } }) =>
+  data.filter((record) => matchCondition(record, _or).includes(true));
 
-exports._or = ({ data, queries: { _or } }) => {
-  let found = [];
-
-  Array.isArray(_or)
-    ? _or.forEach((condition) => {
-        const match = data.filter((record) =>
-          matchCondition(record, condition).includes(true)
-        );
-
-        found.indexOf(...match) === -1 && found.push(...match);
-      })
-    : (found = data.filter((record) =>
-        matchCondition(record, _or).includes(true)
-      ));
-
-  return found;
-};
-
-exports._and = ({ data, queries: { _and } }) => {
-  Array.isArray(_and)
-    ? _and.forEach((condition) => {
-        data = data.filter(
-          (record) => !matchCondition(record, condition).includes(false)
-        );
-      })
-    : (data = data.filter(
-        (record) => !matchCondition(record, _and).includes(false)
-      ));
-
-  return data;
-};
+exports._and = ({ data, queries: { _and } }) =>
+  data.filter((record) => !matchCondition(record, _and).includes(false));
